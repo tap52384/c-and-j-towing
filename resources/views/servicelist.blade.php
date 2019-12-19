613a11268294d4aaa839130028f6bb913e787976
Z. Patrick Lewis <patrick_lewis@unc.edu>
2019-12-19 09:35:37 -0500

    creating service list for views

---
+++ resources/views/servicelist.blade.php
@@ -0,0 +1,16 @@
+@php
+
+$serviceList = [
+    '24-Hour Towing' => [
+        'anchor-name' => '24-hour-towing',
+        'catchy-desc-dark' => '',
+        'catchy-desc-light' => '',
+        'details' => '',
+        'font-awesome' => 'fa-clock-o',
+        'full-name' => '24-Hour Towing'
+    ],
+    'Battery Replacement' => [
+    ]
+];
+
+@endphp
