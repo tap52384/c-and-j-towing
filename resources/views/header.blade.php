a681fcaa46916006219ef28ebc19ead8a47f91f1
Z. Patrick Lewis <patrick_lewis@unc.edu>
2019-12-20 09:19:01 -0500

    added url for easy icon searches

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -6,8 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <!-- Font Awesome 4.7.0 CDN -->
+        <!-- https://fontawesome.com/v4.7.0/icons/ -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-        <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/fontawesome.min.css" /> -->
 
         <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
 
