c473876c9b3134aab9ae91f572343658e5ae155c
Z. Patrick Lewis <patrick_lewis@unc.edu>
2019-12-20 09:16:49 -0500

    services now loaded using array across site

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,62 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $serviceList = [
+            '24-hour-towing' => [
+                'anchor-name' => '24-hour-towing',
+                'catchy-desc-dark' => 'Flatbed towing.',
+                'catchy-desc-light' => '24 hours a day, every day.',
+                'details' => '',
+                'font-awesome' => 'fa-clock-o',
+                'display-name' => '24-Hour Towing'
+            ],
+            'battery-replacement' => [
+                'anchor-name' => 'battery-replacement',
+                'catchy-desc-dark' => 'Battery died?',
+                'catchy-desc-light' => 'A new one is a call away.',
+                'details' => 'If you suspect your battery is having problems, ' .
+                'we can test it within minutes and jumpstart your vehicle. If ' .
+                'you need a new one, we can sell you one and install it for ' .
+                'you on the spot.',
+                'font-awesome' => 'fa-battery',
+                'display-name' => 'Battery Replacement'
+            ],
+            'fuel-delivery' => [
+                'anchor-name' => 'fuel-delivery',
+                'catchy-desc-dark' => 'Tank empty?',
+                'catchy-desc-light' => 'We deliver both gas and diesel.',
+                'details' => '',
+                'font-awesome' => 'fa-battery',
+                'display-name' => 'Fuel Delivery & Refill'
+            ],
+            'locksmith-service' => [
+                'anchor-name' => 'locksmith-service',
+                'catchy-desc-dark' => 'Lockout recovery.',
+                'catchy-desc-light' => 'See for yourself.',
+                'details' => '',
+                'font-awesome' => 'fa-unlock-alt',
+                'display-name' => 'Locksmith Service & Car Unlocking'
+            ],
+            'roadside-service' => [
+                'anchor-name' => 'roadside-service',
+                'catchy-desc-dark' => '',
+                'catchy-desc-light' => '',
+                'details' => '',
+                'font-awesome' => 'fa-road',
+                'display-name' => 'Roadside Service'
+            ],
+            'tire-repair' => [
+                'anchor-name' => 'tire-repair',
+                'catchy-desc-dark' => '',
+                'catchy-desc-light' => '',
+                'details' => '',
+                'font-awesome' => 'fa-battery',
+                'display-name' => 'Tire Repair & Replacement'
+            ]
+        ];
+
+        // Share this array with all views
+        // https://laravel.com/docs/5.8/views#sharing-data-with-all-views
+        View::share('serviceList', $serviceList);
     }
 }
---
+++ resources/views/featurette-text.blade.php
@@ -0,0 +1,5 @@
+<div class="col-md-7">
+    <h2 class="featurette-heading">{{ $darkText ?? '' }} <span class="text-muted">{{ $lightText ?? '' }}</span>
+    </h2>
+    <p class="lead">{{ $details ?? '' }}</p>
+</div>
